d1ad827da454f954dc665883b5a90ab6138e02de
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2024-12-28 14:29:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -55,7 +55,6 @@ I can't find the <b>{}</b> in my database! 🥲
 ©️ Powered by: <b>{message.chat.title.mention}</b>"""
 
     FILE_CAPTION = """<i>{file_name}</i>
-    ‣ ʙᴏᴛ : <a href=https://t.me/{}>{}</a>
 
 🚫 ᴘʟᴇᴀsᴇ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ᴄʟᴏsᴇ ʙᴜᴛᴛᴏɴ ɪꜰ ʏᴏᴜ ʜᴀᴠᴇ sᴇᴇɴ ᴛʜᴇ ᴍᴏᴠɪᴇ 🚫"""
 
